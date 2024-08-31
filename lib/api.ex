1dff3d12a3000cf9c2c5d7d0da07da862c2640c0
beatrizmontenegro07 <beatriz.m.maia07@gmail.com>
2024-08-31 17:04:45 -0300

    consulta de url por meio do id

--- lib/api.ex
+++ lib/api.ex
@@ -23,10 +23,12 @@ defmodule Api do
       #{String.pad_trailing("Pub Date", 0)}\n"
     IO.puts cabecalho
     show_jobs(job_list)
+    consultJob(job_list)
   end
 
   defp show_jobs([]), do: []
-  defp show_jobs([job | rest]) do
+  defp show_jobs(l) do
+    [job | rest] = l
     id = job["id"]
     company = job["companyName"]
     title = job["jobTitle"]
@@ -46,6 +48,39 @@ defmodule Api do
 
     IO.puts(texto)
     show_jobs(rest)
+  end
+
+  defp consultJob (l) do
+    IO.puts("\nDo you want more information about a job? [Y/N]")
+    ans = IO.gets("") |> String.upcase() |> String.trim()
+
+    cond do
+      ans == "Y" ->
+        IO.puts("\nEnter the ID of the job you want more information about: ")
+        id = IO.gets("") |> String.trim()
+        job = Enum.find(l, fn job -> to_string(job["id"]) == id end)
+
+        case job do
+          nil ->
+            IO.puts("Job not found!")
+
+
+          _->
+            url = job["url"]
+            IO.puts "More information at: #{url}"
+
+        end
+
+        consultJob(l)
+
+      ans == "N" ->
+        IO.puts("Exiting...")
+        :ok
+
+      true ->
+        IO.puts("Enter a valid value!")
+        consultJob(l)
+    end
   end
 
 end
